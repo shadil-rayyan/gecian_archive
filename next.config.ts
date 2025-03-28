217f620fe6cd307a31dd8ac1bc46370a51eba34c
Shadil-rayyan <shadilrayyan2@gmail.com>
2025-03-28 12:15:26 +0530

    allowing crop to fix firbase auth in deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
-  images:{
-    unoptimized:true,
-  }
+  images: {
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin-allow-popups" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" }, // Optional, for better security
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
